f79b6554c56cdf04cc6670963a499b99b2fde9eb
kevindives <kevinsagm@gmail.com>
2022-12-05 17:35:43 -0500

    se agrega animacion para ocultar/mostar la topAppBar cuando se hace scroll y se agrega swipeRefresh

--- app/src/main/java/com/magicworld/superheroesdi/list/SuperheroesListScreen.kt
+++ app/src/main/java/com/magicworld/superheroesdi/list/SuperheroesListScreen.kt
@@ -1,21 +1,30 @@
 package com.magicworld.superheroesdi.list
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,65 +35,112 @@ import com.magicworld.superheroesdi.viewmodel.ListViewModel
 
 @Composable
 fun SuperheroesListScreen(listViewModel: ListViewModel) {
-    
+    val scrollState = rememberLazyListState()
     Scaffold(
-        topBar = {TopAppBarList("Superheroes")}
+        topBar = { TopAppBarList("Superheroes", listViewModel, scrollState) }
     ) {
         Box(modifier = Modifier
-            .fillMaxSize()) {
+            .fillMaxSize()
+            .padding(it)) {
 
-            BodyList(listViewModel)
+            BodyList(listViewModel, scrollState)
         }
     }
 }
 
 @Composable
-fun TopAppBarList( title: String) {
+fun TopAppBarList(title: String, listViewModel: ListViewModel, scrollState: LazyListState) {
 
-        TopAppBar(
-            title = { ListTitle( title ){  } },
-            backgroundColor = aliceAzul,
-            modifier = Modifier
-                .padding(13.dp)
-                .clip(RoundedCornerShape(100)),
-            navigationIcon = {
-                IconButton(onClick = { }
-                ) {
-                    Icon(Icons.Outlined.Menu, null, tint = Color.DarkGray)
-                }
-            },
-            actions = {
+    val scrollUpState = listViewModel.scrollUp.observeAsState()
 
-                IconButton(onClick = { }) {
-                    Image(
-                        painter = painterResource(id = R.drawable.oscurologo),
-                        contentDescription = "",
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .size(45.dp)
-                    )
-                }
+    listViewModel.updateScrollPosition(scrollState.firstVisibleItemIndex)
+
+    val position by animateFloatAsState(if (scrollUpState.value == true) -150f else 0f)
+
+    TopAppBar(
+        title = { ListTitle(title) { } },
+        backgroundColor = aliceAzul,
+        modifier = Modifier
+            .padding(13.dp)
+            .clip(RoundedCornerShape(100))
+            .graphicsLayer { translationY = (position) },
+        navigationIcon = {
+            IconButton(onClick = { }
+            ) {
+                Icon(Icons.Outlined.Menu, null, tint = Color.DarkGray)
             }
-        )
+        },
+        actions = {
 
-}
-
-@Composable
-fun ListTitle( title: String , onTitleClicked: () -> Unit) {
-    Text(text = title , color = Color.DarkGray, fontSize = 15.sp , modifier = Modifier.clickable { onTitleClicked() })
-}
-
-
-@Composable
-fun BodyList(listViewModel: ListViewModel) {
-    listViewModel.getSuperheroesFromServer()
-    val list by listViewModel.onSuperheroesLoaded.observeAsState(arrayListOf())
-    LazyColumn(Modifier.fillMaxWidth()){
-        items(list){ superheroe->
-            ItemUser( superheroe)
+            IconButton(onClick = { }) {
+                Image(
+                    painter = painterResource(id = R.drawable.oscurologo),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(45.dp)
+                )
+            }
         }
+    )
+
+}
+
+@Composable
+fun ListTitle(title: String, onTitleClicked: () -> Unit) {
+    Text(text = title,
+        color = Color.DarkGray,
+        fontSize = 15.sp,
+        modifier = Modifier.clickable { onTitleClicked() })
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun BodyList(listViewModel: ListViewModel, scrollState: LazyListState) {
+
+    listViewModel.getSuperheroesFromServer()
+
+    val list by listViewModel.onSuperheroesLoaded.observeAsState(arrayListOf())
+    val refreshing by listViewModel.isRefreshing.collectAsState()
+    val pullRefreshState = rememberPullRefreshState(refreshing, { listViewModel.refresh() })
+
+    //other way
+    /*val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = refreshing)
+
+    SwipeRefresh(
+        state = swipeRefreshState,
+        onRefresh = listViewModel::refresh,
+        indicator = { state, refreshTrigger ->
+            SwipeRefreshIndicator(
+                state = state,
+                refreshTriggerDistance = refreshTrigger,
+                backgroundColor = Color.Blue,
+                contentColor = Color.LightGray
+            )
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            state = scrollState
+        ) {
+            items(list) { superheroe ->
+                ItemUser(superheroe)
+            }
+        }
+    }*/
+    Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            state = scrollState
+        ) {
+            items(list) { superheroe ->
+                ItemUser(superheroe)
+            }
+        }
+        PullRefreshIndicator(refreshing = refreshing, state = pullRefreshState, Modifier.align(
+            Alignment.TopCenter))
     }
-    
+
 }
 
 @Composable
--- app/src/main/java/com/magicworld/superheroesdi/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/magicworld/superheroesdi/viewmodel/ListViewModel.kt
@@ -8,11 +8,43 @@ import com.magicworld.superheroesdi.model.SuperheroeItem
 import com.magicworld.superheroesdi.repository.ListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ListViewModel @Inject constructor(private val listRepository: ListRepository) : ViewModel() {
+
+    private var lastScrollIndex = 0
+
+    private val _scrollUp = MutableLiveData(false)
+    val scrollUp: LiveData<Boolean>
+        get() = _scrollUp
+
+    fun updateScrollPosition(newScrollIndex: Int) {
+        if (newScrollIndex == lastScrollIndex) return
+
+        _scrollUp.value = newScrollIndex > lastScrollIndex
+        lastScrollIndex = newScrollIndex
+    }
+
+    private val _isLoading = MutableStateFlow(false)
+    val isRefreshing = _isLoading.asStateFlow()
+
+    init {
+        refresh()
+    }
+
+    fun refresh(){
+        viewModelScope.launch {
+            _isLoading.value = true
+            delay(3000L)
+            _isLoading.value = false
+        }
+    }
+
 
     private var superheroesLoad: MutableLiveData<ArrayList<SuperheroeItem>> = MutableLiveData()
     val onSuperheroesLoaded: LiveData<ArrayList<SuperheroeItem>> = superheroesLoad
